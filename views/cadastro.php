84a4774113bde62828478162ad57639d0163b189
ssistemas <santiago.l.monteiro@gmail.com>
2015-07-08 10:51:02 -0300

    dsdsds

--- views/cadastro.php
+++ views/cadastro.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Cadastro</title>
+	<title>Cadastro de Aracoiaba</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/cad.css" />
 	<link rel="stylesheet" href="css/cadado.css" />
