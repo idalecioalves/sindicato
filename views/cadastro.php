d322140eeb6ef7ed03d15c719bdc4df1f7dd9e7b
ssistemas <santiago.l.monteiro@gmail.com>
2015-07-09 19:46:47 -0300

    ??

--- views/cadastro.php
+++ views/cadastro.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />	
 	<script src="http://code.jquery.com/jquery-1.8.2.js"></script>
 	<script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>	
-	<script src="<?php echo baseUrl().'js/script.js'?>"></script>	
+	<script src="<?php echo baseUrl().'/js/script.js'?>"></script>	
 	<script src="./js/cpf.js"></script>
 	<script src="./js/porcima.js"></script>
 	<link rel="stylesheet" href="css/cad.css" />
