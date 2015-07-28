f48f2f402e9926a195715f6e9ea9a7132ceaa657
ssistemas <santiago.l.monteiro@gmail.com>
2015-07-28 18:39:45 -0300

    refactory all

--- routes/routesUsuario.php
+++ routes/routesUsuario.php
@@ -1,46 +1,90 @@
 <?php
-$app->get('/usuario/cadastro', function () use ($app) {
-	$app->render("cadastroUsuario.php");
-});
 
-$app->post('/usuario/cadastro',function() use ($app){	
-	//pega valores do form
-	$nome = $app->request->post('nome'); //ok
-	$email =$app->request->post('email'); //ok
-	$senha = $app->request->post('senha'); //ok    
-	$repsenha = $app->request->post('repsenha'); //ok
-	$nivel = $app->request->post('nivel'); //ok
+$app->group('/usuario', function () use ($app)
+{	
+	$app->get('/lista', function () use ($app)
+	{	
+		$flash = $app->view()->getData('flash');	
 
-	if(!filter_var($email,FILTER_VALIDATE_EMAIL))
+		if(isset($flash['email']))
+		{	
+			$data['email_value'] = $flash['email'];			
+		}
+		if(isset($flash['nome']))
+		{	
+			$data['nome_value'] = $flash['nome'];			
+		}
+		if(isset($flash['nivel']))
+		{	
+			$data['nivel_value'] = $flash['nivel'];			
+		}
+		if(isset($flash['errors']['password_confirm']))
+		{	
+			$data['errors'][] = $flash['errors']['password_confirm'];			
+		}
+		if(isset($flash['errors']['error_email']))
+		{	
+			$data['errors'][] = $flash['errors']['error_email'];			
+		}		
+		$data['lista'] = $app->banco->usuario->order('id desc');
+		$data['nivel'] = [1=>'Administrador',2=>'usuário'];
+		$app->render("cadastroUsuario.php",$data);
+	});
+
+	$app->post('/lista', function () use ($app) 
 	{
-		$app->flash('error', 'email invalido!.');
-		$app->view->setData($data['campo']=['name'=>$nome,'email'=>$email]);
-		$app->render("cadastroUsuario.php",$data);	
-	}
-	//validando a senha e confirma senha
-	if ($senha!=$repsenha)
+		$errors = array();
+
+		$nome = $app->request->post('nome'); 
+		$email =$app->request->post('email');
+		$senha = $app->request->post('senha'); 
+		$repsenha = $app->request->post('repsenha'); 
+		$nivel = $app->request->post('nivel'); 		
+
+		if(!filter_var($email,FILTER_VALIDATE_EMAIL))
+		{		 
+			$errors['error_email'] = "As senhas não são iguais!.";	
+		}
+
+		if ($senha!=$repsenha)
+		{
+			$errors['password_confirm'] = "As senhas não são iguais!.";						
+		}
+
+	/**
+	 * para implementar
+	 * rotina valiada se o emeil ja foi cadastrado
+	 ******************************************************/
+
+	if (count($errors) > 0)
 	{
-		$app->flash('error', 'As senhas não são iguais!.');
-		$app->view->setData($data['campo']=['name'=>$nome,'email'=>$email]);		
-		$app->render("cadastroUsuario.php",$data);		
+		$app->flash('nome', $nome);
+		$app->flash('email', $email);
+		$app->flash('nivel', $nivel);
+		$app->flash('errors', $errors);
+		$app->redirect(baseUrl().'/usuario/lista');
 	}
-	
+
 	$data = array(
-	'name'=> $nome, //name referente ao banco;
-	'email'=>$email,//email referente ao banco;
-	'password'=>$senha,//password referente ao banco;
-	'type_id'=>$nivel,
-	);	
+		'name'=> $nome, 
+		'email'=>$email,
+		'password'=>$senha,
+		'type_id'=>$nivel,
+		);	
 
-	//salva os registo no banco
-	$app->banco->usuario->insert($data);
+
+	//$app->banco->usuario->insert($data);
 	$app->flash('success', 'Registro inserido com suscesso.');
 	//$data['lista']=$app->banco->comunidade->order('id desc');
 	//$app->view->setData($data);
-	$app->redirect(baseUrl().'/usuario/cadastro');
+	$app->redirect(baseUrl().'/usuario/lista');
 
 });
 
+$app->get('/edita/:id', function ($id) use ($app) {});
+$app->post('/edita/:id', function ($id) use ($app) {});
+$app->get('/delete/:id', function ($id) use ($app) {});
 
 
+});
 
