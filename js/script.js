d7000bc9e925a2f9d2a8e0543b97a72bc332f329
ssistemas <santiago.l.monteiro@gmail.com>
2015-07-29 15:35:48 -0300

    all

--- js/script.js
+++ js/script.js
@@ -34,3 +34,12 @@ $(document).ready(function(){
 		$("#loading").hide();
 	});
 });
+
+window.setTimeout(function()
+{
+	$(".alert").fadeTo(500, 0).slideUp(500, function(){
+		$(this).remove();
+	});
+}, 5000);
+
+
